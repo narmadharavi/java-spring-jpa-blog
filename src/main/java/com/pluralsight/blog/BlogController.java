098e08b1eb8292f378eb9dc5ec265fe3ff34df0b
narmadharavi <narmadha826@gmail.com>
2021-01-19 16:40:53 +0000

    Module3_Tasks

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -16,14 +16,16 @@ import java.util.Optional;
 public class BlogController {
 
     private PostRepository postRepository;
+    private CategoryRepository categoryRepository;
 
-    public BlogController(PostRepository postRepository) {
+    public BlogController(PostRepository postRepository, CategoryRepository categoryRepository) {
         this.postRepository = postRepository;
     }
 
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
         List<Post> posts = postRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
         modelMap.put("posts", posts);
         return "home";
     }
@@ -33,5 +35,14 @@ public class BlogController {
         Post post = postRepository.findById(id).orElse(null);
         modelMap.put("post", post);
         return "post-details";
+    }
+
+    @RequestMapping("/category/{id}")
+    public String categoryList(@PathVariable Long id, ModelMap modelMap){
+        Category category = categoryRepository.findById(id).orElse(null);
+        modelMap.put("category", category);
+        List<Post> posts = postRepository.findByCategory(category);
+        List<Category> categories = categoryRepository.findAll();
+        return "category-list";
     }
 }
